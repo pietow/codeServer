c9b556707e77eff944a9c713b56ee71139f072a2
piet <pprotassow@posteo.de>
2022-08-26 13:45:05 +0200

    feat: AsyncQueue Class

--- server.js
+++ server.js
@@ -4,43 +4,27 @@ const app = require('express')()
 const port = process.env.PORT || 3000
 const { resolve } = require('path')
 const { readdir } = require('fs/promises')
+const AsyncQueue = require('./asyncQueue.js')
 
-/* async function* getFiles(dir) { */
-/* const dirents = await readdir(dir, { withFileTypes: true }) */
-/* for (const dirent of dirents) { */
-/*     /1* const res = resolve(dir, dirent.name) *1/ */
-/*     const res = dir + '/' + dirent.name */
-/*     if (dirent.isDirectory() && dirent.name !== '.git') { */
-/*         yield* getFiles(res) */
-/*     } else { */
-/*         yield res */
-/*     } */
-/* } */
-/* } */
+const queue = new AsyncQueue()
 
-//asynchronous for/await loop
-async function getFiles(dir, arr) {
-    const dirents = await readdir(dir, { withFileTypes: true })
-    for await (const dirent of dirents) {
-        const res = resolve(dir, dirent.name)
-        if (dirent.isDirectory() && dirent.name !== '.git') {
-            arr.push(res)
-            await getFiles(res, arr)
-        } else {
-            arr.push(res)
-        }
-    }
-    return arr
+function getFiles(dir) {
+    readdir(dir, { withFileTypes: true }).then((files) => {
+        files.forEach((file) => {
+            queue.enqueue(dir + '/' + file.name)
+            file.isDirectory() &&
+                file.name !== '.git' &&
+                getFiles(dir + '/' + file.name)
+        })
+    })
 }
-;(async function () {
-    console.log(await getFiles('../codeSharer/', []))
-})()
 
-/* ;(async () => { */
-/*     for await (const f of getFiles('../codeSharer')) { */
-/*         console.log(f) */
-/*     } */
-/* })() */
+getFiles('../codeSharer')
+;(async function () {
+    for await (const file of queue) {
+        console.log('>', file)
+    }
+})()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
