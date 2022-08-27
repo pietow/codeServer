f0f1dfa1eef8cb9ea56be6dc3b2c88be2b447dc0
piet <pprotassow@posteo.de>
2022-08-27 02:39:31 +0200

    feat:socket.io server without cors

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
 /** @format */
 
 const app = require('express')()
-const port = process.env.PORT || 3000
-const { resolve } = require('path')
+const port = process.env.PORT || 8080
+
 const { readdir } = require('fs/promises')
 const AsyncQueue = require('./asyncQueue.js')
 
+const http = require('http')
+const server = http.createServer(app)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    },
+})
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    io.emit('code', 'Hello World')
+})
+
 const queue = new AsyncQueue()
 
-function getFiles(dir) {
+;(function getFiles(dir = '../codeSharer') {
     readdir(dir, { withFileTypes: true }).then((files) => {
         files.forEach((file) => {
-            queue.enqueue(dir + '/' + file.name)
             file.isDirectory() &&
-                file.name !== '.git' &&
-                getFiles(dir + '/' + file.name)
+            file.name !== '.git' &&
+            file.name !== 'node_modules'
+                ? getFiles(dir + '/' + file.name)
+                : queue.enqueue(dir + '/' + file.name)
         })
     })
-}
-
-getFiles('../codeSharer')
+})()
 ;(async function () {
     for await (const file of queue) {
-        console.log('>', file)
+        /* console.log('>', file) */
     }
 })()
 
@@ -30,6 +47,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
